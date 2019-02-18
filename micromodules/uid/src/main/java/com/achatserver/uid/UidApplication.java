af1bb79f764bdfdbee23a060687f711079b1e557
qianxuekui <daipinghua@grtman.com>
2019-02-18 18:17:12 +0800

    add eureka

--- micromodules/uid/src/main/java/com/achatserver/uid/UidApplication.java
+++ micromodules/uid/src/main/java/com/achatserver/uid/UidApplication.java
@@ -4,7 +4,9 @@ import com.achatserver.uid.thread.ThreadMap;
 import com.achatserver.uid.thread.ThreadQueue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class UidApplication {
     public static void main(String[] args) {
--- micromodules/user/src/main/java/com/achatserver/user/UserApplication.java
+++ micromodules/user/src/main/java/com/achatserver/user/UserApplication.java
@@ -2,12 +2,14 @@ package com.achatserver.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import util.AchatUtil;
 import util.IdWorker;
 import util.Jwt;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class UserApplication {
     public static void main(String[] args) {
